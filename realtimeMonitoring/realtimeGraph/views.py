3ec8f7e02315ca8acb5be3023443bed1d30e0b9e
Luis Leon <l.leon@uniandes.edu.co>
2022-07-11 23:58:56 -0500

    Incluir consulta nueva

--- realtimeMonitoring/realtimeGraph/views.py
+++ realtimeMonitoring/realtimeGraph/views.py
@@ -564,6 +564,76 @@ def get_map_json(request, **kwargs):
     return JsonResponse(data_result)
 
 
+def get_map_json_data(request, **kwargs):
+    data_result = {}
+
+    measureParam = kwargs.get("measure", None)
+    selectedMeasure = None
+    measurements = Measurement.objects.all()
+
+    if measureParam != None:
+        selectedMeasure = Measurement.objects.filter(name=measureParam)[0]
+    elif measurements.count() > 0:
+        selectedMeasure = measurements[0]
+
+    users = User.objects.all()
+    try:
+        start = datetime.fromtimestamp(
+            float(request.GET.get("from", None)) / 1000
+        )
+    except:
+        start = None
+    try:
+        end = datetime.fromtimestamp(
+            float(request.GET.get("to", None)) / 1000)
+    except:
+        end = None
+    if start == None and end == None:
+        start = datetime.now()
+        start = start - dateutil.relativedelta.relativedelta(weeks=1)
+        end = datetime.now()
+        end += dateutil.relativedelta.relativedelta(days=1)
+    elif end == None:
+        end = datetime.now()
+    elif start == None:
+        start = datetime.fromtimestamp(0)
+
+    data = []
+
+    start_ts = int(start.timestamp() * 1000000)
+    end_ts = int(end.timestamp() * 1000000)
+
+    for user in users:
+        stations = Station.objects.filter(user=user)
+        locationData = Data.objects.filter(
+            station__in=stations, measurement__name=selectedMeasure.name, time__gte=start_ts, time__lte=end_ts,
+        )
+        if locationData.count() <= 0:
+            continue
+        minVal = locationData.aggregate(Min("min_value"))["min_value__min"]
+        maxVal = locationData.aggregate(Max("max_value"))["max_value__max"]
+        avgVal = locationData.aggregate(Avg("avg_value"))["avg_value__avg"]
+        data.append(
+            {
+                'login': user.login,
+                'email': user.email,
+                "population": stations.count(),
+                "min": minVal if minVal != None else 0,
+                "max": maxVal if maxVal != None else 0,
+                "avg": round(avgVal if avgVal != None else 0, 2),
+            }
+        )
+
+    startFormatted = start.strftime("%d/%m/%Y") if start != None else " "
+    endFormatted = end.strftime("%d/%m/%Y") if end != None else " "
+
+    data_result["start"] = startFormatted
+    data_result["end"] = endFormatted
+    data_result["data"] = data
+
+    return JsonResponse(data_result)
+
+
 class RemaView(TemplateView):
     template_name = "rema.html"
 
